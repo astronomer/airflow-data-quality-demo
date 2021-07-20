c6f9dff9e05e0984c0196b0b34d64b8508012b9e
Benji Lampel <benjamin@astronomer.io>
2021-07-20 12:53:51 -0400

    Edit the error checking in simple_el_2 dag to use the redshift error table instead of commit table, as this gives the actual upload error information and will ensure errors are caught. Not the best solution as-is, really only works for this sample use-case, but it should be pretty easily extensible to real use-cases. Most notably, we are assuming that the most recent query with a specific filename is the one we want, which may not always be a safe assumption, but generally (and especially with timestamped files), should either be a safe assumption or the query can be modified to use the timestamp to ensure the correct file(s) is/are being checked.

--- dags/simple_el_2.py
+++ dags/simple_el_2.py
@@ -1,4 +1,4 @@
-from airflow import DAG
+from airflow import DAG, AirflowException
 from airflow.decorators import task
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.email import EmailOperator
@@ -14,11 +14,13 @@ import logging
 
 # The file(s) to upload shouldn't be hardcoded in a production setting, this is just for demo purposes.
 CSV_FILE_NAME = "forestfires.csv"
-CSV_FILE_PATH = f"sample_data/{CSV_FILE_NAME}"
-CSV_CORRUPT_FILE_PATH = "sample_data/forestfires_corrupt.csv"
+CSV_FILE_PATH = f"include/sample_data/{CSV_FILE_NAME}"
+CSV_CORRUPT_FILE_NAME = "forestfires_corrupt.csv"
+CSV_CORRUPT_FILE_PATH = f"include/sample_data/{CSV_CORRUPT_FILE_NAME}"
 AWS_CONFIGS = Variable.get("aws_configs", deserialize_json=True)
 S3_BUCKET = AWS_CONFIGS.get("s3_bucket")
-S3_KEY = AWS_CONFIGS.get("s3_key_prefix") + "/" + CSV_FILE_PATH
+# Change CSV_FILE_PATH to CSV_CORRUPT_FILE_PATH below to see the "sad path".
+S3_KEY = AWS_CONFIGS.get("s3_key_prefix") + "/" + CSV_CORRUPT_FILE_PATH
 REDSHIFT_TABLE = AWS_CONFIGS.get("redshift_table")
 
 # These args will get passed on to each operator
@@ -77,15 +79,12 @@ with DAG("simple_el_dag_2",
         s3 = S3Hook()
         obj = s3.get_key(key=S3_KEY, bucket_name=S3_BUCKET)
         obj_etag = obj.e_tag.strip('"')
-        # Change `CSV_FILE_PATH` to `CSV_CORRUPT_FILE_PATH` for the "sad path".
         file_hash = hashlib.md5(open(CSV_FILE_PATH).read().encode("utf-8")).hexdigest()
-        if obj_etag == file_hash:
-            return "load_to_redshift"
-        return "s3_load_fail"
+        if obj_etag != file_hash:
+            raise AirflowException(f"Upload Error: Object ETag in S3 did not match hash of local file.")
 
     upload_files = upload_to_s3()
     validate_s3_file = validate_etag()
-
 
     """
     #### Create Redshift Table (Optional)
@@ -122,26 +121,44 @@ with DAG("simple_el_dag_2",
         #### Redshift row validation task
         Ensure that data was copied to Redshift from S3 correctly.
         """
-        get_stl_load_commit = """SELECT query, trim(filename) as filename, curtime, status
-                                 FROM stl_load_commits
-                                 WHERE filename LIKE '%{filename}%' ORDER BY query;
-                                 """.format(filename=CSV_FILE_NAME)
+        stl_load_errors_query = """
+            SELECT
+              query,
+              trim(filename) as filename,
+              line_number,
+              colname,
+              raw_line,
+              raw_field_value,
+              err_code,
+              err_reason
+            FROM stl_load_errors
+            WHERE filename LIKE '%{filename}%'
+            ORDER BY query DESC
+            LIMIT 1;
+        """.format(filename=CSV_FILE_NAME)
         pg_hook = PostgresHook(postgres_conn_id="redshift_default")
         connection = pg_hook.get_conn()
         cursor = connection.cursor()
-        cursor.execute(get_stl_load_commit)
-        stl_load_commits = cursor.fetchall()
-        logging.info(f"stl load commits: {stl_load_commits}")
-        stl_load_commits.sort(key=lambda x: x[0], reverse=True)
-        logging.info(f"last upload: {stl_load_commits[0]}")
-        return "valid" if stl_load_commits[0][3] == 1 else "redshift_load_fail"
+        cursor.execute(stl_load_errors_query)
+        stl_load_errors = cursor.fetchall()
+        if stl_load_errors:
+            for stl_load_error in stl_load_errors:
+                query, filename, line_number, colname, raw_line, raw_field_value, err_code, err_reason = stl_load_error
+                raise AirflowException(
+                    f"File {filename} errored during upload query number {query} from S3 to Redshift\n"
+                    f"Error occured on line {line_number} and column {colname.rstrip()}\n"
+                    f"Error code: {err_code}\n"
+                    f"Error reason: {err_reason.rstrip()}\n"
+                    f"Raw line: {raw_line.rstrip()}\n"
+                    f"Raw field value: {raw_field_value.rstrip()}"
+                )
 
     validate_redshift = validate_redshift_table()
 
-    valid = DummyOperator(task_id="valid")
+    done = DummyOperator(task_id="done")
 
     upload_files >> validate_s3_file
     validate_s3_file >> create_redshift_table
     create_redshift_table >> load_to_redshift
     load_to_redshift >> validate_redshift
-    validate_redshift >> valid
+    validate_redshift >> done
