1294093ac8ced20f7b2d97dac611b58f5bbb6959
Benji Lampel <benjamin@astronomer.io>
2021-07-21 12:46:22 -0400

    Clean up el_3 dag to match el_2 and el_1 changes, largely regarding DAG structure and usage of Variables. Fixed forestfires_invalid.csv so it correctly uploads to redshift, but will fail the data quality checks.

--- dags/simple_el_3.py
+++ dags/simple_el_3.py
@@ -18,14 +18,14 @@ import logging
 # The file(s) to upload shouldn't be hardcoded in a production setting, this is just for demo purposes.
 CSV_FILE_NAME = "forestfires.csv"
 CSV_FILE_PATH = f"include/sample_data/{CSV_FILE_NAME}"
-CSV_CORRUPT_FILE_NAME = "forestfires_corrupt.csv"
-CSV_CORRUPT_FILE_PATH = f"include/sample_data/{CSV_CORRUPT_FILE_NAME}"
-CSV_INVALID_FILE_NAME = "forestfires_invalid.csv"
-CSV_INVALID_FILE_PATH = f"include/sample_data/{CSV_INVALID_FILE_NAME}"
-AWS_CONFIGS = Variable.get("aws_configs", deserialize_json=True)
-S3_BUCKET = AWS_CONFIGS.get("s3_bucket")
-S3_KEY = AWS_CONFIGS.get("s3_key_prefix") + "/" + CSV_FILE_PATH
-REDSHIFT_TABLE = AWS_CONFIGS.get("redshift_table")
+#CSV_CORRUPT_FILE_NAME = "forestfires_corrupt.csv"
+#CSV_CORRUPT_FILE_PATH = f"include/sample_data/{CSV_CORRUPT_FILE_NAME}"
+
+# Uncomment the below two constants to see the "sad path" - make sure to delete
+# any valid records in Redshift first, or the data quality check will still succeed
+# based on the old records.
+#CSV_FILE_NAME = "forestfires_invalid.csv"
+#CSV_FILE_PATH = f"include/sample_data/{CSV_FILE_NAME}"
 
 # These args will get passed on to each operator
 # You can override them on a per-task basis during operator initialization
@@ -47,23 +47,26 @@ with DAG("simple_el_dag_3",
     This is the third in a series of DAGs showing an EL pipeline with data integrity
     and data quality checking. A single file is uploaded to S3, then its ETag is
     verified against the MD5 hash of the local file. The two should match, which
-    will allow the DAG to continue to the next task. To see the DAG failure case,
-    change `CSV_FILE_PATH` to `CSV_CORRUPT_FILE_PATH` in the `upload_to_s3` task.
-    The failure here is caused by missing rows. If the "happy path" is continued,
-    a second data load from S3 to Redshift is triggered, which is followed by
-    another data integrity check. If the check fails, an Airflow Exception is raised.
-    Otherwise, a final data quality check is performed on the Redshift table for
-    the ID column; if this check fails, an Airflow Exception is raised.
+    will allow the DAG to continue to the next task. A second data load from S3
+    to Redshift is triggered, which is followed by another data integrity check.
+    If the check fails, an Airflow Exception is raised. Otherwise, a final data
+    quality check is performed on the Redshift table per row for a subset of rows,
+    immitating a row-based data quality spot check where the specific ground truth
+    is known.
 
-    Before running the DAG, set the following in a Variable:
+    Before running the DAG, set the following in an Airflow or Environment Variable:
     - key: aws_configs
     - value: { "s3_bucket": [bucket_name], "s3_key_prefix": [key_prefix], "redshift_table": [table_name]}
     Fully replacing [bucket_name], [key_prefix], and [table_name].
 
     What makes this a simple data quality case is:
     1. Absolute ground truth: the local CSV file is considered perfect and immutable.
-    2.
-    3. Single metric to validate.
+    2. No transformations or business logic.
+    3. Exact values of data to quality check are known.
+
+    This demo solves the issue simple_el_2 left open: quality checking the data
+    in the uploaded file. This DAG is a good starting point for a data integrity
+    and data quality check.
     """
 
     @task
@@ -72,25 +75,30 @@ with DAG("simple_el_dag_3",
         #### Upload task
         Simply loads the file to a specified location in S3.
         """
+        aws_configs = Variable.get("aws_configs", deserialize_json=True)
+        s3_bucket = aws_configs.get("s3_bucket")
+        s3_key = aws_configs.get("s3_key_prefix") + "/" + CSV_FILE_PATH
         s3 = S3Hook()
-        s3.load_file(CSV_FILE_PATH, S3_KEY, bucket_name=S3_BUCKET, replace=True)
+        s3.load_file(CSV_FILE_PATH, s3_key, bucket_name=s3_bucket, replace=True)
+        return { "s3_bucket": s3_bucket, "s3_key": s3_key }
 
     @task
-    def validate_etag():
+    def validate_etag(s3_data):
         """
         #### Validation task
         Check the destination ETag against the local MD5 hash to ensure the file
         was uploaded without errors.
         """
         s3 = S3Hook()
-        obj = s3.get_key(key=S3_KEY, bucket_name=S3_BUCKET)
+        obj = s3.get_key(key=s3_data.get("s3_key"), bucket_name=s3_data.get("s3_bucket"))
         obj_etag = obj.e_tag.strip('"')
         file_hash = hashlib.md5(open(CSV_FILE_PATH).read().encode("utf-8")).hexdigest()
         if obj_etag != file_hash:
             raise AirflowException(f"Upload Error: Object ETag in S3 did not match hash of local file.")
+        return s3_data
 
-    upload_files = upload_to_s3()
-    validate_s3_file = validate_etag()
+    upload_file = upload_to_s3()
+    validate_file = validate_etag(upload_file)
 
     """
     #### Create Redshift Table (Optional)
@@ -98,13 +106,13 @@ with DAG("simple_el_dag_3",
     The database is not automatically destroyed at the end of the example; ensure
     this is done manually to avoid unnecessary costs. Additionally, set-up may
     need to be done in Airflow connections to allow access to Redshift. This step
-    may also be accomplished manually; it is, then the task should be removed below.
+    may also be accomplished manually; if it is, then the task should be removed below.
     """
     create_redshift_table = PostgresOperator(
         task_id='create_table',
         sql="sql/create_forestfire_table.sql",
         postgres_conn_id="redshift_default",
-        params={"redshift_table": REDSHIFT_TABLE}
+        params={"redshift_table": Variable.get("aws_configs", deserialize_json=True).get("redshift_table")}
     )
 
     """
@@ -113,19 +121,19 @@ with DAG("simple_el_dag_3",
     in the Airflow Variables backend).
     """
     load_to_redshift = S3ToRedshiftOperator(
-        s3_bucket=S3_BUCKET,
-        s3_key=S3_KEY,
+        s3_bucket=Variable.get("aws_configs", deserialize_json=True).get("s3_bucket"),
+        s3_key=f'{Variable.get("aws_configs", deserialize_json=True).get("s3_key_prefix")}/{CSV_FILE_PATH}',
         schema="PUBLIC",
-        table=REDSHIFT_TABLE,
+        table=Variable.get("aws_configs", deserialize_json=True).get("redshift_table"),
         copy_options=['csv'],
-        task_id='load_to_redshift'
+        task_id='load_to_redshift',
     )
 
     def redshift_load_error(cell):
         # If there is a load error, log the query here so it can be checked
         # directly in Redshift for further details.
         logging.info(f"Checking failure case for cell: {cell}")
-        return False #Change back to True!
+        return True
 
     """
     #### Redshift row validation task
@@ -151,7 +159,7 @@ with DAG("simple_el_dag_3",
             ffv_json = json.load(ffv)
             for id, values in ffv_json.items():
                 values["id"] = id
-                values["redshift_table"] = REDSHIFT_TABLE
+                values["redshift_table"] = Variable.get("aws_configs", deserialize_json=True).get("redshift_table")
                 SQLCheckOperator(
                     task_id=f"forestfire_row_quality_check_{id}",
                     conn_id="redshift_default",
@@ -161,8 +169,7 @@ with DAG("simple_el_dag_3",
 
     done = DummyOperator(task_id="done")
 
-    upload_files >> validate_s3_file
-    validate_s3_file >> create_redshift_table
+    validate_file >> create_redshift_table
     create_redshift_table >> load_to_redshift
     load_to_redshift >> validate_redshift
     validate_redshift >> quality_check_group
