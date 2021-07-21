956472fec7f6d44ffc6c6743de294b3056135066
Benji Lampel <benjamin@astronomer.io>
2021-07-21 12:24:57 -0400

    Update simple_el_2 with more documentation and change structure to match simple_el_1 changes. Additionally, made edits to remove Variable gets from top-level code. This makes the code less DRY in the case of recreating the S3 Key, but it does conform to the best practice of no top-level code.

--- dags/simple_el_2.py
+++ dags/simple_el_2.py
@@ -1,13 +1,11 @@
 from airflow import DAG, AirflowException
 from airflow.decorators import task
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.email import EmailOperator
 from airflow.utils.dates import datetime
 from airflow.models import Variable
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
-from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.sensors.sql import SqlSensor
 
 import hashlib
@@ -18,11 +16,6 @@ CSV_FILE_NAME = "forestfires.csv"
 CSV_FILE_PATH = f"include/sample_data/{CSV_FILE_NAME}"
 CSV_CORRUPT_FILE_NAME = "forestfires_corrupt.csv"
 CSV_CORRUPT_FILE_PATH = f"include/sample_data/{CSV_CORRUPT_FILE_NAME}"
-AWS_CONFIGS = Variable.get("aws_configs", deserialize_json=True)
-S3_BUCKET = AWS_CONFIGS.get("s3_bucket")
-# Change CSV_FILE_PATH to CSV_CORRUPT_FILE_PATH below to see the "sad path".
-S3_KEY = AWS_CONFIGS.get("s3_key_prefix") + "/" + CSV_CORRUPT_FILE_PATH
-REDSHIFT_TABLE = AWS_CONFIGS.get("redshift_table")
 
 # These args will get passed on to each operator
 # You can override them on a per-task basis during operator initialization
@@ -50,15 +43,20 @@ with DAG("simple_el_dag_2",
     which is followed by another data integrity check. A similar "happy/sad path"
     branch ends the DAG.
 
-    Before running the DAG, set the following in a Variable:
+    Before running the DAG, set the following in an Airflow or Environment Variable:
     - key: aws_configs
     - value: { "s3_bucket": [bucket_name], "s3_key_prefix": [key_prefix], "redshift_table": [table_name]}
     Fully replacing [bucket_name], [key_prefix], and [table_name].
 
     What makes this a simple data quality case is:
     1. Absolute ground truth: the local CSV file is considered perfect and immutable.
-    2.
-    3. Single metric to validate.
+    2. No transformations or business logic.
+    3. Single metric to validate (whether the uploads were successful).
+
+    This demo solves the issue the simple_el_1 DAG left open: validating an
+    upload to Redshift. However, it only validates that the data matches the
+    source file; it does not guarantee that the source file's data is actually
+    valid with respect to expectations about that data.
     """
 
     @task
@@ -67,25 +65,31 @@ with DAG("simple_el_dag_2",
         #### Upload task
         Simply loads the file to a specified location in S3.
         """
+        aws_configs = Variable.get("aws_configs", deserialize_json=True)
+        s3_bucket = aws_configs.get("s3_bucket")
+        # Change `CSV_FILE_PATH` to `CSV_CORRUPT_FILE_PATH` for the "sad path".
+        s3_key = aws_configs.get("s3_key_prefix") + "/" + CSV_FILE_PATH
         s3 = S3Hook()
-        s3.load_file(CSV_FILE_PATH, S3_KEY, bucket_name=S3_BUCKET, replace=True)
+        s3.load_file(CSV_FILE_PATH, s3_key, bucket_name=s3_bucket, replace=True)
+        return { "s3_bucket": s3_bucket, "s3_key": s3_key }
 
     @task
-    def validate_etag():
+    def validate_etag(s3_data):
         """
         #### Validation task
         Check the destination ETag against the local MD5 hash to ensure the file
         was uploaded without errors.
         """
         s3 = S3Hook()
-        obj = s3.get_key(key=S3_KEY, bucket_name=S3_BUCKET)
+        obj = s3.get_key(key=s3_data.get("s3_key"), bucket_name=s3_data.get("s3_bucket"))
         obj_etag = obj.e_tag.strip('"')
         file_hash = hashlib.md5(open(CSV_FILE_PATH).read().encode("utf-8")).hexdigest()
         if obj_etag != file_hash:
             raise AirflowException(f"Upload Error: Object ETag in S3 did not match hash of local file.")
+        return s3_data
 
-    upload_files = upload_to_s3()
-    validate_s3_file = validate_etag()
+    upload_file = upload_to_s3()
+    validate_file = validate_etag(upload_file)
 
     """
     #### Create Redshift Table (Optional)
@@ -93,13 +97,13 @@ with DAG("simple_el_dag_2",
     The database is not automatically destroyed at the end of the example; ensure
     this is done manually to avoid unnecessary costs. Additionally, set-up may
     need to be done in Airflow connections to allow access to Redshift. This step
-    may also be accomplished manually; it is, then the task should be removed below.
+    may also be accomplished manually; if it is, then the task should be removed below.
     """
     create_redshift_table = PostgresOperator(
         task_id='create_table',
         sql="sql/create_forestfire_table.sql",
         postgres_conn_id="redshift_default",
-        params={"redshift_table": REDSHIFT_TABLE}
+        params={"redshift_table": Variable.get("aws_configs", deserialize_json=True).get("redshift_table")}
     )
 
     """
@@ -108,10 +112,10 @@ with DAG("simple_el_dag_2",
     in the Airflow Variables backend).
     """
     load_to_redshift = S3ToRedshiftOperator(
-        s3_bucket=S3_BUCKET,
-        s3_key=S3_KEY,
+        s3_bucket=Variable.get("aws_configs", deserialize_json=True).get("s3_bucket"),
+        s3_key=f'{Variable.get("aws_configs", deserialize_json=True).get("s3_key_prefix")}/{CSV_FILE_PATH}',
         schema="PUBLIC",
-        table=REDSHIFT_TABLE,
+        table=Variable.get("aws_configs", deserialize_json=True).get("redshift_table"),
         copy_options=['csv'],
         task_id='load_to_redshift',
     )
@@ -138,8 +142,7 @@ with DAG("simple_el_dag_2",
 
     done = DummyOperator(task_id="done")
 
-    upload_files >> validate_s3_file
-    validate_s3_file >> create_redshift_table
+    validate_file >> create_redshift_table
     create_redshift_table >> load_to_redshift
     load_to_redshift >> validate_redshift
     validate_redshift >> done
