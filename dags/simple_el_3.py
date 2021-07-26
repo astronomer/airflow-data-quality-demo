9f40781485ad51a9d66d867f07fec5a59734f85b
Benji Lampel <benjamin@astronomer.io>
2021-07-26 17:11:17 -0400

    Remove unused SQLSensor import.

--- dags/simple_el_3.py
+++ dags/simple_el_3.py
@@ -7,7 +7,6 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.sensors.sql import SqlSensor
 from airflow.operators.sql import SQLCheckOperator
 from airflow.utils.task_group import TaskGroup
 
