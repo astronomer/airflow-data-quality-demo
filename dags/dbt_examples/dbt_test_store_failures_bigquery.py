68a5a255fc61c7e020d7afca8dd6ffa38f713ff2
Benjamin <denimalpaca@gmail.com>
2021-11-23 11:40:09 -0500

    Added Snowflake version of dbt store_failures copy DAG
    Renamed DAGs and cleaned up bigquery version to not use certain
    imports and operators until AIP-42 is released. Added a snowflake
    version of the DAG which can also use the FFMC test. Runs dbt run
    then dbt test, with 2 tests designed to fail. The outputs are then
    loaded to a permanent table in the original schema. Modifications
    to the directory structure to support these two separate DAGs as
    their own dbt projects, as they work slightly differently.

--- dags/dbt_examples/dbt_test_store_failures_bigquery.py
+++ dags/dbt_examples/dbt_test_store_failures_bigquery.py
@@ -1,23 +1,20 @@
 import os
-import ast
 
 from airflow.utils.dates import datetime
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from airflow.providers.google.cloud.operators.bigquery import (
-    BigQueryGetDatasetTablesOperator, BigQueryInsertJobOperator)
-from airflow.providers.google.cloud.transfers.bigquery_to_bigquery import BigQueryToBigQueryOperator
+from airflow.providers.google.cloud.transfers.bigquery_to_bigquery import (
+    BigQueryToBigQueryOperator)
 
 
-DBT_PROJ_DIR = os.getenv('DBT_PROJECT_DIR')
+DBT_PROJ_DIR = os.getenv('DBT_PROJECT_DIR_BIGQUERY')
 DBT_PROFILE_DIR = os.getenv('DBT_PROFILE_DIR')
 DATASET = 'simple_bigquery_example_dag'
 AUDIT_PATH = f'{DATASET}_dbt_test__audit'
 FAIL_TABLE = 'accepted_values_forestfire_test_month__aug__mar__sep'
 
-with DAG('dbt_test_store_failures',
+with DAG('dbt.copy_store_failures_bigquery',
          start_date=datetime(2021, 10, 8),
-         template_searchpath='/usr/local/airflow/include/sql/dbt_examples/',
          schedule_interval=None) as dag:
     """
     DAG to run dbt project and tests, then load the store_failures table into
@@ -54,26 +51,18 @@ with DAG('dbt_test_store_failures',
     )
 
     """
-    Get the store_failures tables from dbt test failures
-    """
-    get_dataset_tables = BigQueryGetDatasetTablesOperator(
-        task_id='get_dataset_tables',
-        dataset_id=AUDIT_PATH,
-        trigger_rule='one_failed'
-    )
-
-    """
     Copy data from each store_failures table
 
     Until (AIP-42)[https://cwiki.apache.org/confluence/display/AIRFLOW/AIP-42%3A+Dynamic+Task+Mapping]
     is implemented, each task must be hard-coded. One is given as an example.
     """
     copy_store_failures = BigQueryToBigQueryOperator(
-        task_id=f'copy_store_failures',
+        task_id='copy_store_failures',
         source_project_dataset_tables=f'{AUDIT_PATH}.{FAIL_TABLE}',
         destination_project_dataset_table=f'{AUDIT_PATH}_permanent.{FAIL_TABLE}',
         write_disposition='WRITE_APPEND',
-        create_disposition='CREATE_IF_NEEDED'
+        create_disposition='CREATE_IF_NEEDED',
+        trigger_rule='one_failed'
     )
 
-    dbt_run >> dbt_test >> get_dataset_tables >> copy_store_failures
+    dbt_run >> dbt_test >> copy_store_failures
