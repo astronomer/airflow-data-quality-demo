8fc3ee8bedb1870c0ce525017fb5e29abc8535a8
Benjamin <denimalpaca@gmail.com>
2021-11-30 10:28:52 -0500

    Fix ffmc value check query and add it back to the schema

--- include/dbt/forestfire_dq_bigquery/macros/ffmc_value_check.sql
+++ include/dbt/forestfire_dq_bigquery/macros/ffmc_value_check.sql
@@ -1,7 +1,8 @@
 {% test ffmc_value_check(model, column_name) %}
 
 SELECT {{ column_name }}
-FROM simple_bigquery_example_dag.{{ model }}
+FROM {{ model }}
+GROUP BY {{ column_name }}
 HAVING NOT({{ column_name }} >= 90)
 
 {% endtest %}
