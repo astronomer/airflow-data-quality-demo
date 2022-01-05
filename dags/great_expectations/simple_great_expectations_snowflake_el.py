6b614168e9a35bcf88aef13e616e4e43b5f78d6d
Benjamin <denimalpaca@gmail.com>
2022-01-05 10:07:59 -0500

    Fix tasks in Snowflake DAG that weren't being scheduled. Got rid of one of the days as it is supposed to fail tests, and this test should only succeed.

--- dags/great_expectations/simple_great_expectations_snowflake_el.py
+++ dags/great_expectations/simple_great_expectations_snowflake_el.py
@@ -24,10 +24,8 @@ from include.great_expectations.configs.snowflake_configs import (
 
 # This table variable is a placeholder, in a live environment, it is better
 # to pull the table info from a Variable in a template
-TABLE = "YELLOW_TRIPDATA"
-DATES = ["2019-01", "2019-02"]
-TASK_DICT = {}
-SNOWFLAKE_CONN_ID = "snowflake_default"
+table = "YELLOW_TRIPDATA"
+snowflake_conn = "snowflake_default"
 
 base_path = Path(__file__).parents[2]
 expectation_file = os.path.join(
@@ -49,18 +47,17 @@ with DAG(
     description="Example DAG showcasing loading and data quality checking with Snowflake and Great Expectations.",
     schedule_interval=None,
     default_args={
-        # TODO: update connection get here
-        "snowflake_conn_id": SNOWFLAKE_CONN_ID,
-        "warehouse": json.loads(BaseHook.get_connection(SNOWFLAKE_CONN_ID).extra)[
+        "snowflake_conn": snowflake_conn,
+        "warehouse": json.loads(BaseHook.get_connection(snowflake_conn).extra)[
             "extra__snowflake__warehouse"
         ],
-        "database": json.loads(BaseHook.get_connection(SNOWFLAKE_CONN_ID).extra)[
+        "database": json.loads(BaseHook.get_connection(snowflake_conn).extra)[
             "extra__snowflake__database"
         ],
-        "role": json.loads(BaseHook.get_connection(SNOWFLAKE_CONN_ID).extra)[
+        "role": json.loads(BaseHook.get_connection(snowflake_conn).extra)[
             "extra__snowflake__role"
         ],
-        "schema": BaseHook.get_connection(SNOWFLAKE_CONN_ID).schema,
+        "schema": BaseHook.get_connection(snowflake_conn).schema,
     },
     template_searchpath=f"{base_path}/include/sql/great_expectations_examples/",
     catchup=False,
@@ -70,8 +67,8 @@ with DAG(
 
     Ensure a Snowflake Warehouse, Database, Schema, Role, and S3 Key and Secret
     exist for the Snowflake connection, named `snowflake_default`. Access to S3
-    is needed for this example. A staging table may need to be created in
-    Snowflake manually.
+    is needed for this example. An 'aws_configs' variable is needed in Variables,
+    see the Redshift Examples in the README section for more information.
 
     What makes this a simple data quality case is:
     1. Absolute ground truth: the local CSV file is considered perfect and immutable.
@@ -83,6 +80,19 @@ with DAG(
     end = DummyOperator(task_id="end")
 
     """
+    #### Upload task
+    Simply loads the file to a specified location in S3.
+    """
+    upload_to_s3 = LocalFilesystemToS3Operator(
+        task_id="upload_to_s3",
+        filename=data_file,
+        dest_key="{{ var.json.aws_configs.s3_key_prefix }}/yellow_tripdata_sample_2019-01.csv",
+        dest_bucket="{{ var.json.aws_configs.s3_bucket }}",
+        aws_conn_id="aws_default",
+        replace=True,
+    )
+
+    """
     #### Snowflake table creation
     Create the table to store sample data.
     """
@@ -90,8 +100,8 @@ with DAG(
         task_id="create_snowflake_table",
         sql="{% include 'create_yellow_tripdata_snowflake_table.sql' %}",
         params={
-            "table_name": TABLE,
-            "schema": BaseHook.get_connection(SNOWFLAKE_CONN_ID).schema,
+            "table_name": table,
+            "schema": BaseHook.get_connection(snowflake_conn).schema,
         },
     )
 
@@ -102,7 +112,7 @@ with DAG(
     delete_snowflake_table = SnowflakeOperator(
         task_id="delete_snowflake_table",
         sql="{% include 'delete_yellow_tripdata_table.sql' %}",
-        params={"table_name": TABLE},
+        params={"table_name": table},
         trigger_rule="all_done",
     )
 
@@ -114,27 +124,10 @@ with DAG(
     load_to_snowflake = S3ToSnowflakeOperator(
         task_id="load_to_snowflake",
         prefix="test/tripdata",
-        stage=f"{TABLE}_STAGE",
-        table=TABLE,
+        stage=f"{table}_STAGE",
+        table=table,
         file_format="(type = 'CSV', skip_header = 1, time_format = 'YYYY-MM-DD HH24:MI:SS')",
     )
-
-    for i, date in enumerate(DATES):
-        file_name = f"yellow_tripdata_sample_{date}.csv"
-        file_path = f"{data_dir}/{file_name}"
-
-        """
-        #### Upload task
-        Simply loads the file to a specified location in S3.
-        """
-        TASK_DICT[f"upload_to_s3_{date}"] = LocalFilesystemToS3Operator(
-            task_id=f"upload_to_s3_{date}",
-            filename=file_path,
-            dest_key="{{ var.json.aws_configs.s3_key_prefix }}/tripdata/" + file_name,
-            dest_bucket="{{ var.json.aws_configs.s3_bucket }}",
-            aws_conn_id="aws_default",
-            replace=True,
-        )
 
     """
     #### Great Expectations suite
@@ -148,6 +141,7 @@ with DAG(
 
     chain(
         begin,
+        upload_to_s3,
         create_snowflake_table,
         load_to_snowflake,
         ge_snowflake_validation,
